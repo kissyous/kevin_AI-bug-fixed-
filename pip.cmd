9ea390b18b18537c2a4623059f116774ad3791f8
kissyous <85719906+kissyous@users.noreply.github.com>
2025-05-04 22:41:22 +0800

    Add files via upload
    一键安装依赖包，但你要确保你已安装pip。

---
+++ pip.cmd
@@ -0,0 +1,13 @@
+pip install --upgrade pip
+pip install requests
+pip install beautifulsoup4
+pip install lxml
+pip install pandas
+pip install openpyxl
+pip install openai
+pip install pyautogui
+pip install tkinter
+pip install keyboard
+pip install threading
+start en.py
+
